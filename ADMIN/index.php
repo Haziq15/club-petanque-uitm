c15e470332645def9739f810f92094fb63ec7b18
Haziq15 <98736447+Haziq15@users.noreply.github.com>
2024-02-08 12:50:03 +0800

    Add files via upload

---
+++ ADMIN/index.php
@@ -0,0 +1,440 @@
+<?php
+include "db_conn.php";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>CLUB PETANQUE PSKK UiTMCK</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
+</style>
+<!-- Bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body class="w3-light-grey w3-content" style="max-width:1600px">
+
+<!-- Sidebar/menu -->
+<nav class="w3-sidebar w3-collapse w3-white w3-animate-left" style="z-index:3;width:300px;" id="mySidebar"><br>
+  <div class="w3-container">
+    <a href="#" onclick="w3_close()" class="w3-hide-large w3-right w3-jumbo w3-padding w3-hover-grey" title="close menu">
+      <i class="fa fa-remove"></i>
+    </a>
+    <img src="photo/image9.jpg" style="width:45%;" class="w3-round"><br><br>
+    <h4><b>CLUB PETANQUE PSKK</b></h4>
+    <p class="w3-text-grey">UiTM Cawangan Kelantan</p>
+  </div>
+  <div class="w3-bar-block">
+    <a href="#portfolio" onclick="w3_close()" class="w3-bar-item w3-button w3-padding w3-text-teal"><i class="fa fa-th-large fa-fw w3-margin-right"></i>HOME</a> 
+	<a href="#about" onclick="w3_close()" class="w3-bar-item w3-button w3-padding"><i class="fa fa-user fa-fw w3-margin-right"></i>ABOUT CLUB PETANQUE PSKK</a> 
+	<a href="#committee" onclick="w3_close()" class="w3-bar-item w3-button w3-padding"><i class="fa fa-user fa-fw w3-margin-right"></i>COMMITTEE</a> 
+	<a href="#membership" onclick="w3_close()" class="w3-bar-item w3-button w3-padding"><i class="fa fa-user fa-fw w3-margin-right"></i>MEMBERSHIP</a>
+<a href="#activity" onclick="w3_close()" class="w3-bar-item w3-button w3-padding"><i class="fa fa-user fa-fw w3-margin-right"></i>ACTIVITY</a> 	
+    <a href="#achievements" onclick="w3_close()" class="w3-bar-item w3-button w3-padding"><i class="fa fa-user fa-fw w3-margin-right"></i>ACHIEVEMENTS</a> 
+	<a href="#club" onclick="w3_close()" class="w3-bar-item w3-button w3-padding"><i class="fa fa-user fa-fw w3-margin-right"></i>CLUB</a> 
+    <a href="#contact" onclick="w3_close()" class="w3-bar-item w3-button w3-padding"><i class="fa fa-envelope fa-fw w3-margin-right"></i>CONTACT</a>
+	<a href="ADMIN/index.php" onclick="w3_close()" class="w3-bar-item w3-button w3-padding w3-text-teal"><i class="fa fa-th-large fa-fw w3-margin-right"></i>ADMINISTRATOR</a> 
+  </div>
+  
+</nav>
+
+<!-- Overlay effect when opening sidebar on small screens -->
+<div class="w3-overlay w3-hide-large w3-animate-opacity" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
+
+<!-- !PAGE CONTENT! -->
+<div class="w3-main" style="margin-left:300px">
+
+  <!-- Header -->
+  <header id="portfolio">
+    <a href="#"><img src="/w3images/avatar_g2.jpg" style="width:65px;" class="w3-circle w3-right w3-margin w3-hide-large w3-hover-opacity"></a>
+    <span class="w3-button w3-hide-large w3-xxlarge w3-hover-text-grey" onclick="w3_open()"><i class="fa fa-bars"></i></span>
+    <div class="w3-container">
+    <h1><b>WELCOME TO CLUB PETANQUE PSKK UiTMCK</b></h1>
+   
+
+      
+
+  <!-- Images of Me -->
+  <div class="w3-row-padding w3-padding-16" id="about">
+    <div class="w3-col m6">
+      <p align="center"><img src="PHOTO/image10.jpg" alt="Me" style="width:75%"></p>
+    </div>
+   
+ <!--about club petanque pskk -->
+  <div class="w3-container w3-padding-large" style="margin-bottom:32px">
+    <h4><b>CLUB PETANQUE PSKK </b></h4>
+    <p>The Club de Pétanque PSKK at UiTM Machang, Kelantan, likely operates within the university campus to provide students and possibly faculty members with opportunities to participate in the sport of pétanque. Being situated at UiTM Machang suggests that it's part of the extracurricular activities offered by the university.
+
+As with any pétanque club, its primary focus is likely on promoting the game, fostering a sense of community among members, and perhaps even competing in intervarsity or local tournaments. It could serve as a platform for students to unwind, socialize, and engage in healthy competition outside of their academic pursuits. Additionally, it might organize training sessions, friendly matches, and other events to encourage participation and skill development among its members.</p>
+    <hr>
+	
+	  <!-- Committee -->
+<div id="committee" class="w3-container w3-padding-64 w3-center" >
+<h2>ORGANIZATIONAL CHART</h2>
+<p>AJK</p>
+
+<div class="w3-row"><br>
+
+<div class="w3-quarter">
+  <img src="photo/image1.jpg" alt="Boss" style="width:45%" class="w3-SQUARE w3-hover-opacity">
+  <h3>MUHD AMIR RAHMAN</h3>
+  <p>LEADER</p>
+</div>
+
+<div class="w3-quarter">
+  <img src="photo/image2.jpg" alt="Boss" style="width:45%" class="w3-SQUARE w3-hover-opacity">
+  <h3>HASAN SALEH</h3>
+  <p>VICE LEADER</p>
+</div>
+
+
+<div class="w3-quarter">
+  <img src="photo/image3.jpg" alt="Boss" style="width:45%" class="w3-SQUARE w3-hover-opacity">
+  <h3>AINA QISTINA</h3>
+  <p>SECRETARY</p>
+</div>
+
+<div class="w3-quarter">
+  <img src="photo/image4.jpg" alt="Boss" style="width:45%" class="w3-SQUARE w3-hover-opacity">
+  <h3>RAISHA HANA</h3>
+  <p>VICE SECRETARY</p>
+</div>
+
+
+<div class="w3-row"><br>
+
+<div class="w3-quarter">
+  <img src="photo/image5.jpg" alt="Boss" style="width:45%" class="w3-SQUARE w3-hover-opacity">
+  <h3>HAKIM IMRAN</h3>
+  <p>COORDINATOR EVENT</p>
+</div>
+
+<div class="w3-quarter">
+  <img src="photo/image6.jpg" alt="Boss" style="width:45%" class="w3-SQUARE w3-hover-opacity">
+  <h3>AIN SOLEHA</h3>
+  <p>FINANCE MANAGER</p>
+</div>
+
+<div class="w3-quarter">
+  <img src="photo/image7.jpg" alt="Boss" style="width:45%" class="w3-SQUARE w3-hover-opacity">
+  <h3>CIK AMIRUL</h3>
+  <p>TEAM MANAGER</p>
+</div>
+
+<div class="w3-quarter">
+  <img src="photo/image8.jpg" alt="Boss" style="width:45%" class="w3-SQUARE w3-hover-opacity">
+  <h3>CIK ADAM</h3>
+  <p>ADVISOR</p>
+</div>
+
+</div>
+</div>
+    
+    <h4><B>CLUB PETANQUE PSKK MEMBERSHIP</B></h4>
+    <!-- Progress bars / Skills -->
+	<div id="membership" class="w3-container w3-padding-64 w3-center" >
+    <?php
+    if (isset($_GET["msg"])) {
+      $msg = $_GET["msg"];
+      echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
+      ' . $msg . '
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>';
+    }
+    ?>
+	
+	
+	
+	<p align="center"><h3>Membership form for club petanque pskk 2024</h3></p>
+    <a href="add-new.php" class="btn btn-dark mb-3">Add New</a>
+
+    <table class="table table-hover text-center">
+      <thead class="table-dark">
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">First Name</th>
+          <th scope="col">Last Name</th>
+          <th scope="col">Email</th>
+          <th scope="col">Gender</th>
+         
+        </tr>
+      </thead>
+      <tbody>
+	 
+        <?php
+        $sql = "SELECT * FROM `members`";
+        $result = mysqli_query($conn, $sql);
+        while ($row = mysqli_fetch_assoc($result)) {
+        ?>
+          <tr>
+            <td><?php echo $row["id"] ?></td>
+            <td><?php echo $row["first_name"] ?></td>
+            <td><?php echo $row["last_name"] ?></td>
+            <td><?php echo $row["email"] ?></td>
+            <td><?php echo $row["gender"] ?></td>
+            
+          </tr>
+        <?php
+        }
+        ?>
+      </tbody>
+    </table>
+  </div>
+  
+  <!-- activity -->
+  <div id="activity" class="w3-container w3-padding-64 w3-center" >
+   <p id="ACTIVITY" align="center"><h3>Activities form for club petanque pskk </h3></p>
+    <a href="add_activity.php" class="btn btn-dark mb-3">Add New</a>
+
+    <table class="table table-hover text-center">
+      <thead class="table-dark">
+        <tr>
+          <th scope="col">Activity ID</th>
+          <th scope="col">Activity Name</th>
+          <th scope="col">Activity Date</th>
+          <th scope="col">Club ID</th>
+          
+        </tr>
+      </thead>
+      <tbody>
+   
+        <?php
+        $sql = "SELECT * FROM `activity`";
+        $result = mysqli_query($conn, $sql);
+        while ($row = mysqli_fetch_assoc($result)) {
+        ?>
+          <tr>
+            <td><?php echo $row["activity_id"] ?></td>
+            <td><?php echo $row["activity_name"] ?></td>
+            <td><?php echo $row["activity_date"] ?></td>
+            <td><?php echo $row["club_id"] ?></td>
+            <td>
+             
+          </tr>
+        <?php
+        }
+        ?>
+      </tbody>
+    </table>
+  </div>
+
+  <!-- Bootstrap -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+</body>
+    
+    <div class="container">
+    <?php
+    if (isset($_GET["msg"])) {
+      $msg = $_GET["msg"];
+      echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
+      ' . $msg . '
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>';
+    }
+   
+ ?>
+<hr>
+	<!-- Achievements -->
+<div id="achievements" class="w3-container w3-padding-64 w3-center" >
+  <h1>Achievements club petanque pskk
+</h1>
+</div>
+
+<div class="w3-row-padding w3-margin-top">
+  <div class="w3-third">
+    <div class="w3-card">
+      <img src="photo/image11.jpg" style="width:100%">
+      <div class="w3-container">
+        <h5>Perfect Precision: Achieved by consistently landing boules within a centimeter of the target over 20 rounds.
+Excelling in accuracy, mastering the art of precise throws in every game and competition.</h5>
+      </div>
+    </div>
+  </div>
+
+  <div class="w3-third">
+    <div class="w3-card">
+      <img src="photo/image12.jpg" style="width:100%">
+      <div class="w3-container">
+        <h5>Triple Triumph: Awarded for winning three consecutive interclub tournaments, showcasing remarkable consistency and skill.
+Dominating the competition, demonstrating unparalleled prowess in consecutive interclub showdowns.</h5>
+      </div>
+    </div>
+  </div>
+
+  <div class="w3-third">
+    <div class="w3-card">
+      <img src="photo/image13.jpg" style="width:100%">
+      <div class="w3-container">
+        <h5>Community Champions: Recognized for organizing charity events, bringing joy through petanque while supporting local causes.
+Uniting the community, spreading goodwill through petanque, and making a positive impact together.</h5>
+      </div>
+    </div>
+  </div>
+</div>
+
+</body>
+
+<!-- club -->
+<div id="club" class="w3-container w3-padding-64 w3-center" >
+  <p id="Club Collaboration" align="center"><h3>Club form Club Petanque Pskk</h3></p>
+    <a href="add_club.php" class="btn btn-dark mb-3">Add New</a>
+
+    <table class="table table-hover text-center">
+      <thead class="table-dark">
+        <tr>
+          <th scope="col">Club ID</th>
+          <th scope="col">Club Name</th>
+          <th scope="col">Club Ownership</th>
+          <th scope="col">Club Type</th>
+         
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        $sql = "SELECT * FROM `club`";
+        $result = mysqli_query($conn, $sql);
+        while ($row = mysqli_fetch_assoc($result)) {
+        ?>
+          <tr>
+            <td><?php echo $row["club_id"] ?></td>
+            <td><?php echo $row["club_name"] ?></td>
+            <td><?php echo $row["club_owner"] ?></td>
+            <td><?php echo $row["club_type"] ?></td>
+          
+            <td>
+             
+            </td>
+          </tr>
+        <?php
+        }
+        ?>
+      </tbody>
+    </table>
+  </div>
+
+  <!-- Bootstrap -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+</body>
+
+   <div id="contact" class="container">
+    <?php
+    if (isset($_GET["msg"])) {
+      $msg = $_GET["msg"];
+      echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
+      ' . $msg . '
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>';
+    }
+    ?>
+
+    <hr>
+	
+    </p>
+    <hr>
+    
+  
+  <!-- Bootstrap -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+</body>
+
+   <div id="contact" class="container">
+    <?php
+    if (isset($_GET["msg"])) {
+      $msg = $_GET["msg"];
+      echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
+      ' . $msg . '
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>';
+    }
+    ?>
+
+    <hr>
+
+<!-- Contact Section -->
+  <div id="contact" class="container">
+    <?php
+    if (isset($_GET["msg"])) {
+      $msg = $_GET["msg"];
+      echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
+      ' . $msg . '
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>';
+    }
+    ?>
+  <p align="center"><h3>Contact Us for UiTMCK Club Information System (UCIS)</h3></p>
+    <a href="add_contact.php" class="btn btn-dark mb-3">Add New</a>
+
+    <table class="table table-hover text-center">
+      <thead class="table-dark">
+        <tr>
+          <th scope="col">Contact ID</th>
+          <th scope="col">Contact Name</th>
+          <th scope="col">Contact Student ID</th>
+          <th scope="col">Email</th>
+          <th scope="col">Contact Number</th>
+          <th scope="col">Comment</th>
+         
+        </tr>
+      </thead>
+      <tbody>
+   
+        <?php
+        $sql = "SELECT * FROM contact";
+        $result = mysqli_query($conn, $sql);
+        while ($row = mysqli_fetch_assoc($result)) {
+        ?>
+          <tr>
+            <td><?php echo $row["contact_id"] ?></td>
+            <td><?php echo $row["contact_name"] ?></td>
+            <td><?php echo $row["contact_studid"] ?></td>
+            <td><?php echo $row["contact_phone"] ?></td>
+            <td><?php echo $row["contact_email"] ?></td>
+            <td><?php echo $row["contact_desc"] ?></td>
+
+            <td>
+             
+            </td>
+          </tr>
+        <?php
+        }
+        ?>
+      </tbody>
+    </table>
+  </div>
+
+  <!-- Bootstrap -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+  
+  
+
+<!-- End page content -->
+</div>
+  
+
+<script>
+// Script to open and close sidebar
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+}
+ 
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+}
+</script>
+
+</body>
+</html>
